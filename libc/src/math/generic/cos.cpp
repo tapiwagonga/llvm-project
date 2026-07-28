41e9b8e8fde01cf9e9c383fc75c7e7f0724427ad
Tapiwa Gonga <tapiwagonga@google.com>
2026-07-28 11:29:19 +0000

    test: inject massive untested dummy blocks into cos and tan for UI stress test

--- libc/src/math/generic/cos.cpp
+++ libc/src/math/generic/cos.cpp
@@ -13,6 +13,35 @@
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(double, cos, (double x)) {
+  // --- EXTREME DUMMY CHANGES START ---
+  if (x == 10001.0) {
+    // Completely uncovered red block
+    double y = x * 2.0;
+    y = y + 1.0;
+    if (y > 20000.0) {
+      return 1.0;
+    } else {
+      return -1.0;
+    }
+  }
+
+  if (x == 0.0) {
+    // This is hit by standard tests! 
+    // This will show up as completely green!
+    double z = 1.0;
+    z = z * z;
+    return z;
+  }
+  
+  if (x == -99999.0) {
+    // Another red branch
+    for(int i = 0; i < 5; i++) {
+       x += 1.0;
+    }
+    return x;
+  }
+  // --- EXTREME DUMMY CHANGES END ---
+
 #if defined(LIBC_MATH_HAS_SKIP_ACCURATE_PASS) &&                               \
     defined(LIBC_MATH_SMALL_TABLES) &&                                         \
     !defined(LIBC_TARGET_CPU_HAS_FPU_DOUBLE)
